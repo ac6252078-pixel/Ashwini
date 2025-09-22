b917112de10bf2547a30bb8703d47f6c0c2a0d0e
ac6252078-pixel <ac6252078@gmail.com>
2025-09-22 14:54:38 +0530

    Table. Py

---
+++ table.py
@@ -0,0 +1,10 @@
+coin=0
+for i in range(1,50):
+    print("go on played ")
+    x=int (input("computer  ask a number "))
+    if(x==20 or x>35):
+        break;
+        print("weldon player ")
+        coin=1
+        print("final coin is=",coin)
+        
